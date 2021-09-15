052f8c110ab1be975cebc61fee369dcbbba86264
UttamVelani <40419289+uttampatel007@users.noreply.github.com>
2021-09-15 08:02:28 +0530

    Update Insertion_Short.py

--- Insertion_Short.py
+++ Insertion_Short.py
@@ -29,4 +29,17 @@ print(A)
 
 
 
+# Insertion sort practice 2
 
+A = [1,5,56,44,2323,54545,3,32,2,2,4,5,6,3,2,23,4,4,6,6,8,9,]
+
+for j in range(1,len(A)):
+    key = A[j]
+    i = j-1
+    
+    while(i>0 and A[i]>key):
+        A[i+1] = A[i]
+        i -= 1
+    A[i+1] = key
+    
+print(A)
