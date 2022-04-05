a585f2934c8bc36a4d23eb17a548f36df94124eb
uttamss11 <uttam.velani@smartsensesolutions.com>
2022-04-05 15:00:43 +0530

    Added tree

---
+++ tree.py
@@ -0,0 +1,43 @@
+from operator import le
+
+
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+    
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
+
+        return level
+
+    def print_tree(self):
+        data = " " * self.get_level() * 3 + "|--" + f"{self.data}"
+        print(data)
+        for child in self.children:
+            child.print_tree()
+
+def build_product_tree():
+    root = TreeNode("Root Node")
+    child = TreeNode("Child Node 1")
+    child2 = TreeNode("Child Node 2")
+
+    root.add_child(child)
+    root.add_child(child2)
+    
+    childchild = TreeNode("Child Child")
+    child.add_child(childchild)
+    root.print_tree()
+
+
+if __name__ == "__main__":
+    build_product_tree()
