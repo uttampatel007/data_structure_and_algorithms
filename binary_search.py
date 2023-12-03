caae0d0185c86914e08c968678a8a1813188e441
UttamVelani <40419289+uttampatel007@users.noreply.github.com>
2023-12-03 10:25:00 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,1 +1,74 @@
-B
+
+tests = [{
+    "input": {
+        "cards":[12,11,10,4,3,2,1],
+        "query": 10
+    },
+    "output": 2
+},{
+    "input": {
+        "cards":[12,11,10,4,3,2,1],
+        "query": 12
+    },
+    "output": 0
+},{
+    "input": {
+        "cards":[12,11,10,4,3,2,1],
+        "query": 1
+    },
+    "output": 6
+},
+{
+    "input": {
+        "cards":[12,11,10,4,3,2,1],
+        "query": 13
+    },
+    "output": -1
+}]
+
+
+def binary_search(cards, query):
+    lo, hi = 0, len(cards)-1
+    
+    while lo <= hi:
+        # find the mid position
+        mid = (lo + hi) // 2
+        mid_num = cards[mid]
+
+        if mid_num == query:
+            return mid
+            
+        elif mid_num > query:
+            lo = mid + 1
+        
+        elif mid_num < query:
+            hi = mid - 1
+    
+    return -1
+
+
+for test in tests:
+    result = binary_search(**test["input"])
+        
+    print(result, test['output'])
+    
+    assert test['output'] == result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
